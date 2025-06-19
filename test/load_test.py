cf7ca12ec29845ef80e62301cf3c76025df74be6
Sipho Zenzile - MpiloTech <sipho.zenzile@mpilo.africa>
2025-06-19 13:32:37 +0200

    modified template and loead test

--- test/load_test.py
+++ test/load_test.py
@@ -4,14 +4,14 @@ import datetime
 import time
 
 # Replace with your actual API Gateway URL
-API_URL = "https://ktu6kd7bml.execute-api.us-east-1.amazonaws.com/Prod/log"
+API_URL = "https://81pavecor6.execute-api.us-east-1.amazonaws.com/Prod/log"
 
 def send_logs():
     for i in range(1, 101):
         payload = {
             "ID": str(i),
             "DateTime": datetime.datetime.utcnow().isoformat() + "Z",
-            "Severity": "info",  # can be "warning" or "error" too
+            "Severity": "info",  
             "Message": f"Test log entry number {i}"
         }
 
@@ -24,7 +24,7 @@ def send_logs():
         print(f"[{i}] Status: {response.status_code} - {response.text}")
 
         # Optional: Delay to avoid spamming API Gateway too quickly
-        time.sleep(0.1)  # 100 ms pause
+        time.sleep(0.1)  # 
 
 if __name__ == "__main__":
     send_logs()
